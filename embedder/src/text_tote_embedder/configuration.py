d140011e78f07a17ceddf0b16c227308918c3f07
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2024-02-25 17:13:08 +0200

    Add more config

--- embedder/src/text_tote_embedder/configuration.py
+++ embedder/src/text_tote_embedder/configuration.py
@@ -4,20 +4,32 @@ from os import path
 
 from decouple import AutoConfig
 
-local_dev_settings_or_test_settings = path.abspath('./settings.ini')
+local_dev_mode = path.exists(path.abspath('./settings.ini'))
+
 user_home = path.expanduser('~/texttote')
-config_root: str = './' if path.exists(local_dev_settings_or_test_settings) else user_home
+pwd = './'
+config_root: str = pwd if local_dev_mode else user_home
+
 os.makedirs(config_root, exist_ok=True)
-config = AutoConfig(search_path=config_root)
+autoconfig = AutoConfig(search_path=config_root)
 
 
 @dataclass
 class AppConfig:
     db_path: str
+    model_name: str
+    model_vector_size: int
 
 
-db_path = path.abspath(path.expanduser(config('DB_PATH', default='~/texttote/texttote.db')))
-
-appconfig = AppConfig(
-    db_path=db_path
+config = AppConfig(
+    db_path=path.abspath(
+        path.expanduser(
+            autoconfig('DB_PATH', default='~/texttote/texttote.db')
+        )
+    ),
+    # TODO compare between msmarco-MiniLM-L-6-v3 and all-MiniLM-L6-v2
+    # https://www.sbert.net/examples/applications/semantic-search/README.html#symmetric-vs-asymmetric-semantic-search
+    # Model msmarco-MiniLM-L-6-v3 chosen because our queries are asymmetric queries
+    model_name=autoconfig('MODEL_NAME', default='all-MiniLM-L6-v2'),
+    model_vector_size=autoconfig('MODEL_VECTOR_SIZE', cast=int, default=384),
 )
